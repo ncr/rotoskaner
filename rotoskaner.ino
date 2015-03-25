3eb8d1c2c8285952ccaf92799b26b05b4a64d271
Jacek Becela <jacek.becela@gmail.com>
2015-03-25 23:57:09 +0100

    Microstep acceleration!

--- rotoskaner.ino
+++ rotoskaner.ino
@@ -10,35 +10,35 @@ Adafruit_StepperMotor *rawStepper = AFMS.getStepper(200, 1);
 Servo myServo;
 
 void forwardstep() {
-  rawStepper->onestep(FORWARD, INTERLEAVE);
+  rawStepper->onestep(FORWARD, MICROSTEP);
 }
 void backwardstep() {  
-  rawStepper->onestep(BACKWARD, INTERLEAVE);
+  rawStepper->onestep(BACKWARD, MICROSTEP);
 }
 
 AccelStepper stepper(forwardstep, backwardstep);
+
+int pos;
 
 void setup() {
   AFMS.begin();  // create with the default frequency 1.6KHz
 //  myMotor->setSpeed(10);  // 10 rpm   
 //  myServo.attach(10);
+  pos = 0;
+  stepper.setMaxSpeed(500);
+  stepper.setAcceleration(100);
 }
 
 void loop() {
   //myMotor->onestep(FORWARD, MICROSTEP); 
   //myMotor->step(1, FORWARD, MICROSTEP); 
 
-    if (stepper.distanceToGo() == 0)
-    {
-	// Random change to speed, position and acceleration
-	// Make sure we dont get 0 speed or accelerations
-	delay(1000);
-	stepper.moveTo(rand() % 200);
-	stepper.setMaxSpeed((rand() % 100) + 100);
-	stepper.setAcceleration((rand() % 200) + 1);
-    }
-    stepper.run();
-
+  if (stepper.distanceToGo() == 0) {
+    pos += 16; // 16 microstepow == 1 stopien
+    delay(1000);
+    stepper.moveTo(pos);
+  }
+  stepper.run();
 
   /*
 
